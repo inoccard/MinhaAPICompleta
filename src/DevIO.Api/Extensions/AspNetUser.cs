147613e8dca824ef52a4548722d307be3322f89b
Inocêncio Cardoso <inocencio.cardoso@iatec.com>
2022-12-13 15:41:25 -0300

    separada classe de usuário e claim

--- src/DevIO.Api/Extensions/AspNetUser.cs
+++ src/DevIO.Api/Extensions/AspNetUser.cs
@@ -39,29 +39,4 @@ namespace DevIO.Api.Extensions
             return _accessor.HttpContext.User.Claims;
         }
     }
-
-    public static class ClaimsPrincipalExtensions
-    {
-        public static string GetUserId(this ClaimsPrincipal principal)
-        {
-            if (principal == null)
-            {
-                throw new ArgumentException(nameof(principal));
-            }
-
-            var claim = principal.FindFirst(ClaimTypes.NameIdentifier);
-            return claim?.Value;
-        }
-
-        public static string GetUserEmail(this ClaimsPrincipal principal)
-        {
-            if (principal == null)
-            {
-                throw new ArgumentException(nameof(principal));
-            }
-
-            var claim = principal.FindFirst(ClaimTypes.Email);
-            return claim?.Value;
-        }
-    }
 }
